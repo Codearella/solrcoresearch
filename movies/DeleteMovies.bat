c372206a9e5e9ab0d6e58291ee13d939e01bbe55
Tara Zieminek <tara.zieminek@gmail.com>
2019-04-24 16:17:18 -0600

    Added scripts to add suggester component. Added autocomplete cdn and code

---
+++ movies/DeleteMovies.bat
@@ -0,0 +1,2 @@
+curl -d "action=DELETE&name=movies" -X POST http://localhost:8983/solr/admin/collections
+curl -d "action=DELETE&name=movies" -X POST http://localhost:8983/solr/admin/configs
--- movies/SetupMovies.bat
+++ movies/SetupMovies.bat
@@ -1,8 +1,8 @@
 
 
 REM ===Try to start the solr nodes, in case they have been stopped.===
-REM ===..\..\bin\solr.cmd start -c -p 8983 -s example/cloud/node1/solr
-REM ===..\..\bin\solr.cmd start -c -p 7574 -s example/cloud/node2/solr -z localhost:9983
+REM ===..\..\bin\solr.cmd start -c -p 8983 -s ..\example\cloud\node1\solr
+REM ===..\..\bin\solr.cmd start -c -p 7574 -s ..\example\cloud\node2\solr -z localhost:9983
 
 REM ===Create a collection called movies.====
 call ..\..\bin\solr create -c movies -s 2 -rf 2
@@ -24,7 +24,26 @@ curl -X POST -H "Content-type:application/json" --data-binary "{\"add-field\":{\
 curl -X POST -H "Content-type:application/json" --data-binary "{\"add-field\":{\"name\":\"tagline\",\"type\":\"text_general\",\"stored\":true,\"multiValued\":false}}" http://localhost:8983/solr/movies/schema
 curl -X POST -H "Content-type:application/json" --data-binary "{\"add-field\":{\"name\":\"title\",\"type\":\"text_general\",\"stored\":true,\"multiValued\":false}}" http://localhost:8983/solr/movies/schema
 curl -X POST -H "Content-type:application/json" --data-binary "{\"add-field\":{\"name\":\"release_date\",\"type\":\"pdate\",\"stored\":true,\"multiValued\":false}}" http://localhost:8983/solr/movies/schema
+
+REM ===Add catch-all field.===
 curl -X POST -H "Content-type:application/json" --data-binary "{\"add-copy-field\" : {\"source\":\"*\",\"dest\":\"_text_\"}}" http://localhost:8983/solr/movies/schema
 
-REM ===Load the data===
+REM ===Define the custom field type for suggest.  Not needed - text general seems to work better. ===
+REM ===curl -X POST -H "Content-type:application/json" --data-binary "{\"add-field-type\":{\"name\":\"suggestType\",\"class\":\"solr.TextField\",\"positionIncrementGap\":\"100\",\"analyzer\":{\"charFilter\":{\"class\":\"solr.PatternReplaceCharFilterFactory\"\"pattern\":\"[^a-zA-Z0-9]\"\"replacement\":\" \"}\"tokenizer\":{\"class\":\"solr.WhitespaceTokenizerFactory\"}\"filter\":{\"class\":\"solr.LowerCaseFilterFactory\"}}}}" http://localhost:8983/solr/movies/schema
+
+
+REM =====================text_general , suggestType
+REM ===Define the suggest component for the index.===
+curl -X POST -H "Content-type:application/json" --data-binary "{\"add-searchcomponent\":{\"name\":\"suggest\",\"class\":\"solr.SuggestComponent\",\"suggester\":{\"name\":\"fuzzySuggester\",\"lookupImpl\":\"FuzzyLookupFactory\",\"storeDir\":\"fuzzy_suggestions\",\"dictionaryImpl\":\"DocumentDictionaryFactory\",\"field\":\"title\",\"weightField\":\"popularity\",\"exactMatchFirst\":\"true\",\"suggestAnalyzerFieldType\":\"text_general\",\"buildOnStartup\":\"false\",\"buildOnCommit\":\"false\"}}}" http://localhost:8983/solr/movies/config
+
+REM ===Define a requestHandler for the suggest component.===     
+curl -X POST -H "Content-type:application/json" --data-binary "{\"add-requesthandler\":{\"name\":\"/suggest\",\"class\":\"solr.SearchHandler\",\"startup\":\"lazy\",\"defaults\":{\"suggest\":\"true\",\"suggest.dictionary\":\"fuzzySuggester\",\"suggest.count\":\"10\",\"suggest.collate\":\"true\",},\"components\":[\"suggest\"]}}" http://localhost:8983/solr/movies/config
+
+REM ===Load the data.===
 java -jar -Dc=movies -Dparams="f.genres.split=true&f.production_companies.split=true&f.genres.separator=|&f.production_companies.separator=|" -Dauto ..\exampledocs\post.jar ..\movies\movies.csv
+
+REM ===Build the suggest component.===   
+curl -d "suggest.build=true" -X POST http://localhost:8983/solr/movies/suggest
+
+
+
